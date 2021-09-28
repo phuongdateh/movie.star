67bbc18d9902b9efe35bb019af8bf29f149cbe53
phuongdoan <95doanphuong@com>
2021-09-28 18:35:54 +0800

    Updated folder

---
+++ core/Scenes/Home/SubViews/Movie/MovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViewCell.swift
+//  core
+//
+//  Created by Doan Duy Phuong on 28/09/2021.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+}
