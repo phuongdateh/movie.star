076439407da85ac2d8aef02a9a1a0dd3c754ef9d
phuongdoan <95doanphuong@com>
2021-10-02 22:34:20 +0800

    Update UI

--- core/Scenes/CreditsDetail/Views/HorizontalMovieView/HorizontalMovieView.swift
+++ core/Scenes/CreditsDetail/Views/HorizontalMovieView/HorizontalMovieView.swift
@@ -13,8 +13,13 @@ final class HorizontalMovieView<T: BaseCollectionViewCell>: UIView,
                                                             UICollectionViewDataSource,
                                                             UICollectionViewDelegateFlowLayout {
     private let stackView = UIStackView()
-    private var collectionView: UICollectionView!
+    
+    private let topStackView = UIStackView()
+    private let viewAllSectionView = UIView()
+    private let viewAllLabel = UILabel()
     private let titleLbl = UILabel()
+    
+    private var collectionView: UICollectionView!
     
     private var movies = [Movie]()
     private var sizeForItem: CGSize = .zero
@@ -40,19 +45,46 @@ final class HorizontalMovieView<T: BaseCollectionViewCell>: UIView,
         collectionView.dataSource = self
     }
     
+    
     private func layout() {
-        stackView.axis = .vertical
-        stackView.backgroundColor = .clear
-        stackView.spacing = 15
+        self.topStackView.addArrangedSubview(titleLbl)
+        self.topStackView.addArrangedSubview(viewAllSectionView)
+        self.viewAllSectionView.addSubview(viewAllLabel)
+        viewAllLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            viewAllLabel.topAnchor.constraint(equalTo: viewAllSectionView.topAnchor),
+            viewAllLabel.rightAnchor.constraint(equalTo: viewAllSectionView.rightAnchor,
+                                                constant: -15),
+            viewAllLabel.bottomAnchor.constraint(equalTo: viewAllSectionView.bottomAnchor)
+        ])
         self.addChildView(view: stackView,
                           topConstant: 20,
                           leftConstant: 15,
                           rightConstant: 15)
-        stackView.addArrangedSubview(titleLbl)
+        stackView.addArrangedSubview(self.topStackView)
         stackView.addArrangedSubview(collectionView)
     }
     
+    @objc private func viewAllButtonTouchUpInside() {
+        print(#function)
+    }
+
     private func configureView() {
+        stackView.axis = .vertical
+        stackView.backgroundColor = .clear
+        stackView.spacing = 15
+
+        topStackView.axis = .horizontal
+        topStackView.spacing = 0
+        topStackView.alignment = .fill
+
+        viewAllLabel.text = "View All"
+        viewAllLabel.font = UIFont(name: AppFont.medium.name, size: 14)
+        viewAllLabel.textColor = .lightGray
+
+        viewAllSectionView.isUserInteractionEnabled = true
+        viewAllSectionView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(HorizontalMovieView.viewAllButtonTouchUpInside)))
+
         titleLbl.font = UIFont(name: AppFont.bold.name, size: 20)
         titleLbl.textColor = .white
         titleLbl.numberOfLines = 1
